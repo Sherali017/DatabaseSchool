23fdb2f3be8dea72658dd9ac5307dd744daffe8a
Sherali017 <sheraliabdurahmonov077@gmail.com>
2021-12-07 11:58:38 +0500

    updated

--- School/settings.py
+++ School/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z1^skh%7=7^v-v$_(=tac)ypmqzl#p5n+p0476(rkwj#nuqamn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
