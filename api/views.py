256a6ffa7dd55feb650fc0990dfd7a2187cce697
Ilyosbek <phyton@micro.domains>
2021-12-07 17:19:17 +0500

    yana

--- api/views.py
+++ api/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, DestroyAPIView, UpdateAPIView, CreateAPIView
 
 from api.serializers import TeacherModelSerializer
 from dbschool.models import TeacherModel
 
 
 class TeacherListAPIView(ListAPIView):
-    queryset = TeacherModel.objects.all()
-    serializer_class = TeacherModelSerializer
+	queryset = TeacherModel.objects.all()
+	serializer_class = TeacherModelSerializer
 
+
+class TeacherCreateAPIView(CreateAPIView):
+	queryset = TeacherModel.objects.all()
+	serializer_class = TeacherModelSerializer
+
+
+class TeacherUpdateAPIView(UpdateAPIView):
+	queryset = TeacherModel.objects.all()
+	serializer_class = TeacherModelSerializer
+
+
+class TeacherDeleteAPIView(DestroyAPIView):
+	queryset = TeacherModel.objects.all()
+	serializer_class = TeacherModelSerializer
--- staff/views.py
+++ staff/views.py
@@ -1,4 +1,3 @@
 from django.shortcuts import render
 
 # class adas(ListView):
-# 	model =
